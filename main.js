f6df90120962941d27f976f7f66e60d8db5e7123
Hongryeoll <hong462804@gmail.com>
2022-11-23 14:58:18 +0900

    index.html 수정

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ function displayMainItems(items){
     const container = document.querySelector('.list-item');
     container.innerHTML = items.map(item=>createMainHtmlString(item)).json('');
 }
+
+// 각 item을 메인 콜라 리스트 html로 변경
+function createMainHtmlString(item){
+    return `
+    <li data-value="${item.color}" class="list-${item.color}">
+    <img
+        src=${item.image}
+        width="36px"
+        height="65px"
+        alt="${item.type}"
+        class="img-${item.color}"
+    />
+    <p class="txt-colaName">${item.type}</p>
+    <p class="txt-price">${item.price}원</p>
+    </li>
+    `
+}
